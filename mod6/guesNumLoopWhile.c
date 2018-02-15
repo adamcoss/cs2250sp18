26ebb5c8b46182fe5bce25f3d509f17bee04d1fe
Adam Coss <adamcoss@mail.weber.edu>
2018-02-15 09:12:50 -0700

    Initial commit

---
+++ mod6/guesNumLoopWhile.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  guesNumLoopWhile.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/15/2018 09:07:28 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Coss (), adamcoss@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
