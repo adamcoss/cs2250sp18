6a8592945786bb4b2b51cea9ecb9a601e0830a3d
Adam Coss <adamcoss@mail.weber.edu>
2018-02-27 08:44:23 -0700

    INITIAL COMMIT

---
+++ mid/adam_coss_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  adam_coss_exam1_task1.c
+ *
+ *    Description:  First choice.
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:04 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Coss (), adamcoss@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mid/adam_coss_exam1_task2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  adam_coss_exam1_task2.c
+ *
+ *    Description:  Second Choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:43 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Coss (), adamcoss@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
