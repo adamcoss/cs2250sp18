4d0acf30235f8bbdff9c9e0196f8e095b1f7fd55
Adam Coss <adamcoss@mail.weber.edu>
2018-03-22 09:36:44 -0600

    INIT COMMIT

---
+++ mod9/seat.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  seat.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/22/2018 09:30:44 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Coss (), adamcoss@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mod9/seat.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  seat.h
+ *
+ *    Description:  :
+ *
+ *        Version:  1.0
+ *        Created:  03/22/2018 09:30:19 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Coss (), adamcoss@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  SEAT__INC__
+#define  SEAT_INC__
+    <+DETAILED+>
+
+#endif /* ----- #ifndef SEAT__INC__ ----- */
+
